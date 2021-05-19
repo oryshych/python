d0b65033705daf0993674dfb7c8d3f1aaf25b770
oryshych <84316457+oryshych@users.noreply.github.com>
2021-05-19 11:08:20 +0300

    range()

---
+++ day-5-3-exercise (adding evens)/main.py
@@ -0,0 +1,13 @@
+#Write your code below this row 👇
+#way1
+total = 0
+for number in range(2, 101, 2):
+  total += number
+print(total)
+
+#way2
+total2 = 0
+for number in range(1, 101):
+  if number %2 == 0:
+    total2 += number
+print(total2)
